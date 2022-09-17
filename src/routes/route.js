b7fdbfa0e710081e5f8c03fa40085b41880e871a
abhi587 <abhishek.pawan14@gmail.com>
2022-09-17 19:25:50 +0530

    done

---
+++ src/routes/route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const CollegeController = require('../controller/collegeController')
+const InternController = require('../controller/internController')
+
+router.post('/functionup/colleges',CollegeController.createCollege)
+
+router.post('/functionup/interns', InternController.createIntern)
+
+router.get('/functionup/collegeDetails',CollegeController.getCollegeDetails)
+
+
+
+
+module.exports = router;
